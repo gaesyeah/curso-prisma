091945ce39be5a402a2b96c0383f9f8640db4566
Gabriel Goes Santos Rios <gabrielgoesrios@gmail.com>
2023-09-20 11:48:33 -0300

    feat: resolução do exercicio 9

---
+++ aula-prisma-09/src/index.ts
@@ -0,0 +1,34 @@
+import { Prisma } from "@prisma/client";
+import express, { Request, Response } from "express";
+import prisma from "./database";
+
+const app = express();
+
+type PetResult = {
+  owner: string;
+  pet: string;
+  animal: string;
+}
+
+app.get("/pets/owner/:ownerName", async (req: Request, res: Response) => {
+  const { ownerName } = req.params;
+  try {
+    const result = await prisma.$queryRaw<PetResult>(
+      Prisma.sql`
+        SELECT people.name as owner, pet.name as pet, pet.type as animal 
+        FROM people
+          JOIN pet ON "personId" = people.id
+        WHERE people.name = ${ownerName}
+      `
+    );
+    res.send(result);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(500);
+  }
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is up and running or port ${port}`);
+})
